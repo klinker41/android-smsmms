2c92d1c47464eb571b9ca8c6248449c474ef8913
aTakayukiMorita <Takayuki.Morita@access-company.com>
2016-12-16 11:15:46 +0900

    Check M-Notification-Ind before download MMS, because TransactionService can keep uri and location while SmsManager download MMS.

--- library/src/main/java/com/android/mms/transaction/DownloadManager.java
+++ library/src/main/java/com/android/mms/transaction/DownloadManager.java
@@ -7,9 +7,13 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
+import android.database.sqlite.SqliteWrapper;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.provider.Telephony;
 import android.telephony.SmsManager;
 import android.text.TextUtils;
 
@@ -39,8 +43,13 @@ public class DownloadManager {
 
     }
 
-    void downloadMultimediaMessage(final Context context, final String location, Uri uri, boolean byPush) {
+    public void downloadMultimediaMessage(final Context context, final String location, Uri uri, boolean byPush) {
         if (location == null || mMap.get(location) != null) {
+            return;
+        }
+
+        // TransactionService can keep uri and location in memory while SmsManager download Mms.
+        if (!isNotificationExist(context, location)) {
             return;
         }
 
@@ -109,5 +118,29 @@ public class DownloadManager {
         if (location != null) {
             mMap.remove(location);
         }
+    }
+
+    private static boolean isNotificationExist(Context context, String location) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return true;
+        }
+
+        String selection = Telephony.Mms.CONTENT_LOCATION + " = ?";
+        String[] selectionArgs = new String[] { location };
+        Cursor c = SqliteWrapper.query(
+                context, context.getContentResolver(),
+                Telephony.Mms.CONTENT_URI, new String[] { Telephony.Mms._ID },
+                selection, selectionArgs, null);
+        if (c != null) {
+            try {
+                if (c.getCount() == 1) {
+                    return true;
+                }
+            } finally {
+                c.close();
+            }
+        }
+
+        return false;
     }
 }
